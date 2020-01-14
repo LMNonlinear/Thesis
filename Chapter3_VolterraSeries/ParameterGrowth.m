56e859389d622d9935a491c9d52b3fe46a941ebf
Jeremy Stoddard <c3164189@uon.edu.au>
2020-01-14 16:50:14 +1100

    Updated to final submission version

--- Chapter3_VolterraSeries/ParameterGrowth.m
+++ Chapter3_VolterraSeries/ParameterGrowth.m
@@ -26,10 +26,20 @@ for nm = lengths
     for m = dimension
         v(m+1) = nchoosek(nm+m-1,m);
     end
-    semilogy(dimension,v,'-o','LineWidth',1.3)
+    switch nm
+        case 10
+            semilogy(dimension,v,'o','LineWidth',1.3,'MarkerSize',7)
+        case 20
+            semilogy(dimension,v,'x','LineWidth',1.3,'MarkerSize',7)
+        case 50
+            semilogy(dimension,v,'*','LineWidth',1.3,'MarkerSize',7)
+        case 100
+            semilogy(dimension,v,'+','LineWidth',1.3,'MarkerSize',7)
+    end
     hold on
 end
 grid on
 xlabel('m')
 ylabel('d_m')
 legend('n_m=10','n_m=20','n_m=50','n_m=100');
+axis([0 5.2 1 2e8])
